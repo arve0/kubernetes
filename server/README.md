44562f65fc504a0fa1dd423677a69598229b1c12
Arve Seljebu <arve.seljebu@gmail.com>
2022-05-19 13:45:55 +0200

    server

---
+++ server/README.md
@@ -0,0 +1,20 @@
+# server
+En enkel server som videresender request til andre servere.
+
+Eksempelvis:
+```sh
+$ node server.js &
+starter server-a på port 8080
+$ PORT=80 NAME=server-b node server.js &
+starter server-b på port 80
+$ curl localhost:8080
+server-a: mottok request fra ::ffff:127.0.0.1 til http://localhost:8080/
+jeg er service server-a
+$ curl localhost:8080/localhost
+server-a: mottok request fra ::ffff:127.0.0.1 til http://localhost:8080/localhost
+server-b: mottok request fra ::ffff:127.0.0.1 til http://localhost/
+jeg er service server-b
+$ pkill -f "node server.js"
+fish: Job 1, 'node server.js &' terminated by signal SIGTERM (Polite quit request)
+fish: Job 2, 'PORT=80 NAME=server-b node serv…' terminated by signal SIGTERM (Polite quit request)
+```
---
+++ server/server.js
@@ -0,0 +1,61 @@
+const http = require("http");
+
+const name = process.env.NAME || "server-a";
+const port = process.env.PORT || "8080";
+
+const server = http.createServer((request, response) => {
+  const forwarded = request.headers["x-forwarded-for"]
+    ? ` forwarded for: ${request.headers["x-forwarded-for"]}`
+    : "";
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  console.log(
+    `${name}: mottok request fra ${request.socket.remoteAddress}${forwarded} til ${url}`
+  );
+
+  if (url.pathname !== "/") {
+    const host = url.pathname
+      .slice(1)
+      .toLowerCase()
+      .replace(/[^a-z]+/g, "");
+
+    http
+      .request({ host, path: "/" }, handleGet(response))
+      .on("error", handleError(response))
+      .end();
+  } else {
+    response.write(`jeg er service ${name}\n`);
+    response.end();
+  }
+});
+
+function handleGet(response) {
+  return function (requestResponse) {
+    response.statusCode = requestResponse.statusCode;
+
+    for (var header in requestResponse.headers) {
+      if (
+        header === "access-control-allow-origin" ||
+        !requestResponse.headers.hasOwnProperty(header)
+      ) {
+        continue;
+      }
+      response.setHeader(header, requestResponse.headers[header]);
+    }
+
+    requestResponse.on("data", response.write.bind(response));
+    requestResponse.on("end", response.end.bind(response));
+  };
+}
+
+function handleError(response) {
+  return function (requestError) {
+    console.error(requestError.stack);
+    // send error
+    response.statusCode = 500;
+    response.write("Error.");
+    response.end();
+  };
+}
+
+console.log(`starter ${name} på port ${port}`);
+server.listen(port);
